4bc9ff9d9dabc84a5ef58d90630c1273bd0032f4
Mustafa-wr <122282675+Mustafa-wr@users.noreply.github.com>
2023-01-18 00:45:57 +0400

    Create atoi_tesr.c

---
+++ atoi_tesr.c
@@ -0,0 +1,17 @@
+int main(void)
+{
+	printf ("\n our atoi : %d  ", ft_atoi("9223372036854775808"));
+	printf("\n original atoi : %d ", atoi("9223372036854775808"));
+	printf ("\n our atoi : %d  ", ft_atoi("9223372036854775800"));
+	printf("\n original atoi : %d", atoi("9223372036854775800"));
+	printf ("\n our atoi : %d  ", ft_atoi("-2147483649000000000000000"));
+	printf("\n original atoi : %d", atoi("-2147483649000000000000000"));
+	printf ("\n our atoi : %d  ", ft_atoi("-2147483649"));
+	printf("\n original atoi : %d", atoi("-2147483649"));
+	printf ("\n our atoi : %d  ", ft_atoi("-0000000000000000000007827666777"));
+	printf("\n original atoi : %d", atoi("-0000000000000000000007827666777"));
+	printf ("\n our atoi : %d  ", ft_atoi("/t/t/t0000000000000000000007827666777"));
+	printf("\n original atoi : %d", atoi("/t/t/t0000000000000000000007827666777"));
+
+	return(0);
+}
